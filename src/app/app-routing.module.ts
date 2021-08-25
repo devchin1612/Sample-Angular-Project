a7281121f1ca04c4e5810e85dfa7b75f4b4c4dd0
venkadeshJ <venkadeshjpm@gmail.com>
2021-08-25 18:05:47 +0530

    Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './navbar/aboutus/aboutus.component';
+import { EmployeesComponent } from './navbar/employees/employees.component';
+import { FeedbackComponent } from './navbar/feedback/feedback.component';
+import { SportsComponent } from './navbar/sports/sports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'employees',component:EmployeesComponent},
+  {path:'sports',component:SportsComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'feedback',component:FeedbackComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[EmployeesComponent,SportsComponent,AboutusComponent,FeedbackComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
